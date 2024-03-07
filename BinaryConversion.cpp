7c3c7d85aabd8a1daac1ccc4225f2a8a7b6921ce
Dizel69 <73982326+Dizel69@users.noreply.github.com>
2024-03-07 19:31:49 +0300

    Добавил программу перевода из десятичной степень в двоичную

---
+++ BinaryConversion.cpp
@@ -0,0 +1,20 @@
+﻿﻿// Переводит число из десятичной степени счислени в двоичную
+
+
+#include <iostream>
+
+int main() {
+	int bin = 0;
+	int n;
+	scanf_s("%d", &n);
+	int rem, i = 1;
+	while (n != 0) {
+		rem = n % 2;
+		n /= 2;
+		bin += rem * i;
+		i *= 10;
+	}
+
+	printf("%d", bin);
+
+}
