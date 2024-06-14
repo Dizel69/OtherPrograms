1a2cbe9618cf8058a49bdf5fbabc97a299a043a8
Dizel69 <dizelking@yandex.ru>
2024-06-14 14:18:21 +0300

    убрал

--- Airflow/dags/dag_with_googleAPI.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# задаем переменные среды́
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\Users\dizel\my_testAirFlow\airflow\client_secret_672637077929-kilu0ecav0gq34l9p8mrhq7icfoe0do7.apps.googleusercontent.com.json"
