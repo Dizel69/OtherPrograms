b576f6ab757ea43ab3eafe6cca47e42311b42a10
Dizel69 <73982326+Dizel69@users.noreply.github.com>
2025-06-15 01:51:52 +0300

    Update leap_year.go

--- Learning_Golang/leap_year.go
+++ Learning_Golang/leap_year.go
@@ -10,7 +10,7 @@ func main() {
 	var a int
 	fmt.Scan(&a)
 
-	if a/400 == 0 || (a/4 == 0 && a/100 != 0) {
+	if a%400 == 0 || (a%4 == 0 && a%100 != 0) {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
